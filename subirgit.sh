09cc96f6f1efbd6bcb1c62f5f3008ffb44cd39b8
Nicolas Mino <nicolasmino@MacBook-Pro-de-Nicolas.local>
2019-09-12 14:07:31 -0300

    Des

---
+++ subirgit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Aautomatizacion de git
+clear
+cat /Users/nicolasmino/Desktop/scripts/banner.txt
+echo ""
+echo ""
+echo "" 
+echo "*************************************      Opciones     *************************************"
+echo "1) Crear repositorio desde 0"
+echo "2) Subir Archivos"
+echo "3) Salir"
+echo ""
+echo "Ingresar Opción"
+read opcion
+if [ $opcion=1 ];
+then
+	echo "Ingrese texto de Repositorio (README)"
+	read texto
+	echo "Ingrese el Nombre de Usuario de GitHub"
+	read usuario
+	echo "Ingrese el Contenido del Commit"
+	read commit 
+	echo "Ingrese el Nombre del Repositorio en GitHub"
+	read repo
+#Comiendo de Comendos
+	echo "#Texto ">> README.md
+	git init
+	git add . 
+	git commit -m "$commit"
+	git remote add origin https://github.com/$usuario/$repo.git
+	git push -u origin master 
+fi
+
