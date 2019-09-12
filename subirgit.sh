1cdc8eb237b77662d7fb5ac102933c7246611a85
Nicolas Mino <nicolasmino@MacBook-Pro-de-Nicolas.local>
2019-09-12 14:17:38 -0300

    first commit

--- subirgit.sh
+++ subirgit.sh
@@ -14,18 +14,18 @@ echo "Ingresar Opción"
 read opcion
 if [ $opcion=1 ];
 then
-	echo "Ingrese texto de Repositorio (README)"
-	read texto
+	echo "Ingrese Texto  de Repositorio (README)"
+	read texto 
 	echo "Ingrese el Nombre de Usuario de GitHub"
 	read usuario
 	echo "Ingrese el Contenido del Commit"
 	read commit 
 	echo "Ingrese el Nombre del Repositorio en GitHub"
 	read repo
-#Comiendo de Comendos
-	echo "#Texto ">> README.md
+	#Comiendo de Comendos
+	echo "# $texto " >> README.md
 	git init
-	git add . 
+	git add README.md 
 	git commit -m "$commit"
 	git remote add origin https://github.com/$usuario/$repo.git
 	git push -u origin master 
